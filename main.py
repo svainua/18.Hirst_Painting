89da1382c8497613960c975a48f0543663354200
svainua <sva.in.ua@gmail.com>
2022-08-07 12:01:26 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import turtle
+from turtle import Turtle, Screen
+import random
+
+turtle.colormode(255)
+tim = Turtle()
+tim.hideturtle()
+tim.penup()
+tim.speed("fastest")
+
+color_list = [(198, 12, 32), (250, 237, 17), (39, 76, 189), (38, 217, 68), (238, 227, 5), (229, 159, 46), (27, 40, 157)]
+
+y = 0
+
+tim.setheading(225)
+tim.forward(250)
+tim.setheading(0)
+number_of_dots = 100
+
+for dot_count in range(1, number_of_dots + 1):
+    tim.dot(20, random.choice(color_list))
+    tim.fd(50)
+
+    if dot_count % 10 == 0 :
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
+
+screen = turtle.Screen()
+screen.exitonclick()
+
